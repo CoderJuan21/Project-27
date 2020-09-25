e54198b74b8cbe7a5e8376c4a5c62926a7b80331
CoderJuan21 <70552194+CoderJuan21@users.noreply.github.com>
2020-09-25 14:27:27 -0400

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,28 @@
+class Bob {
+    constructor(x, y, r) {
+      var options = {
+          isStatic:false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2,
+      }
+
+      this.body = Bodies.circle(x, y, this.r/2, options);
+      this.r = r;
+      World.add(world, this.body);
+    }
+    display(){
+     
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push ();
+      translate(pos.x , pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill(255);
+        ellipse(0, 0, this.r, this.height);
+        pop();
+      }
+    
+  };
+  
